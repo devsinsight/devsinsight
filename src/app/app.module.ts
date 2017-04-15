b6d8ddc9f7df08dd78edf352904dcf6809bb23e7
Jose Olivares <shoutmetal@gmail.com>
2017-04-14 19:11:12 -0500

    divide in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
+],
   imports: [
     BrowserModule,
     FormsModule,
